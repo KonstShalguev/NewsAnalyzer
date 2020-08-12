0141c92a222d001abf95ca60d73054259e5c14e3
Konstantin Shalguev <k.shalguev@yandex.ru>
2020-08-12 08:39:49 +0300

    hooked up swiper

--- src/js/about.js
+++ src/js/about.js
@@ -1,1 +1,22 @@
 import "../styles/about.css";
+
+import Swiper, { Navigation, Pagination } from 'swiper';
+Swiper.use([Navigation, Pagination]);
+
+// import Swiper styles
+import '../../node_modules/swiper/swiper-bundle.min.css';
+
+const swiper = new Swiper('.swiper-container', {
+    slidesPerView: "auto",
+    spaceBetween: 16,
+    //centeredSlides: true,
+    loop: true,
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+    },
+    navigation: {
+    nextEl: '.commits__button-next',
+    prevEl: '.commits__button-prev',
+    },
+});
