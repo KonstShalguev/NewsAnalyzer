66a4daac339f95f0a6467dda2f9a6a1aebe22098
k.shalguev <k.shalguev@yandex.ru>
2022-01-13 21:05:21 +0300

    [k.develop] fix: rewrote code

--- src/index.js
+++ src/index.js
@@ -8,11 +8,6 @@ import {SearchInput} from './js/components/SearchInput';
 import {dateFormatting} from './js/utils/utils';
 import {NEWS_API_KEY, CURRENT_DATE, DATE_WEEK_AGO} from './js/constants/constants';
 
-const newsCard = (imgUrl, title, date, description, source) => {
-  const newsCard = new NewsCard(imgUrl, title, date, description, source, dateFormatting);
-  return newsCard.create()
-}
-
 const buttonShowMore = document.querySelector('.results__button-show-more');
 const searchNotFound = document.querySelector('.not-found');
 const inputSearch = document.querySelector('.input__search');
@@ -20,24 +15,31 @@ const preloader = document.querySelector('.preloader');
 const sectionResults = document.querySelector('.page__results');
 const cardBlock = document.querySelector('.results__cards');
 const formSearch = document.querySelector('.search__form');
-
-//функция отрисовки 
-function loadCards(array) {
-  const arrayWithCards = array;
-  const renderNow = arrayWithCards.articles.splice(0, 3);
-  newsCardList.render(renderNow); //отрисовка карточек с временного масива
-  if (arrayWithCards.articles.length !== 0) {
-    buttonShowMore.style.display = 'block';
-  } else {
-    buttonShowMore.style.display = 'none';
-  }
-  dataStorage.setItem('newsCard', arrayWithCards);
+const newsApi = new NewsApi(NEWS_API_KEY, CURRENT_DATE, DATE_WEEK_AGO);
+const dataStorage = new DataStorage();
+const newsCard = (imgUrl, title, date, description, source) => {
+  const newsCard = new NewsCard(imgUrl, title, date, description, source, dateFormatting);
+  return newsCard.create();
+}
+const newsCardList = new NewsCardList(cardBlock, newsCard);
+if (formSearch) {
+  new SearchInput(formSearch, renderCards, JSON.parse(dataStorage.getItem('keyWord')), '.input__search');
 }
 
-const renderThreeCards = (event) => {
-  event.preventDefault();
+function loadCards(array) {
+  const renderNow = array.articles.splice(0, 3);
+  newsCardList.render(renderNow);
+  if (array.articles.length !== 0) {
+    buttonShowMore.style.display = 'block';
+  }
+  if (array.articles.length === 0) {
+    buttonShowMore.style.display = 'none';
+  }
+  dataStorage.setItem('newsCard', array);
+}
 
-  //обнуляем блоки
+function renderCards(event) {
+  event.preventDefault();
   searchNotFound.style.display = 'none';
   sectionResults.style.display = 'none';
   preloader.style.display = 'block';
@@ -46,42 +48,30 @@ const renderThreeCards = (event) => {
       let keyWord = inputSearch.value.toLowerCase();
       dataStorage.setItem('keyWord', keyWord);
 
-      //копируем масив с карточками
       dataStorage.setItem('newsCardCopy', res);
-      preloader.style.display = 'none'; //обнуляем прелоадер
-      cardBlock.innerHTML = ""; // обнуляем содержимое контейнера
       dataStorage.setItem('newsCard', res);
+      preloader.style.display = 'none';
+      cardBlock.innerHTML = '';
       if (res.totalResults === 0) {
         searchNotFound.style.display = 'block';
-        // удаление предыдущего запроса
-        dataStorage.setItem('keyWord', '')
+        dataStorage.setItem('keyWord', '');
         inputSearch.value = '';
       }
       if (res.totalResults > 0) {
         sectionResults.style.display = 'block';
       }
-      preloader.style.display = 'none'; //обнуляем прелоадер
-      cardBlock.innerHTML = ""; // обнуляем содержимое контейнера
+      preloader.style.display = 'none';
+      cardBlock.innerHTML = '';
       loadCards(JSON.parse(dataStorage.getItem('newsCard')));
     })
-    .catch(err => {
-      console.log(err)
-    })
+    .catch(err => console.log(err))
 }
 
-const newsCardList = new NewsCardList(document.querySelector('.results__cards'), newsCard);
-const newsApi = new NewsApi(NEWS_API_KEY, CURRENT_DATE, DATE_WEEK_AGO);
-const dataStorage = new DataStorage();
-const searchInput = new SearchInput(formSearch, renderThreeCards);
-
-searchInput.setEventListeners();
 buttonShowMore.addEventListener('click', function () {
   loadCards(JSON.parse(dataStorage.getItem('newsCard')));
 })
 
-// отрисовка слова в поле ввода
-inputSearch.value = JSON.parse(dataStorage.getItem('keyWord'));
 if (dataStorage.getItem('newsCardCopy') !== null) {
   sectionResults.style.display = 'block';
   loadCards(JSON.parse(dataStorage.getItem('newsCardCopy')));
-} 
+}
--- src/js/components/SearchInput.js
+++ src/js/components/SearchInput.js
@@ -1,7 +1,16 @@
 export class SearchInput {
-  constructor(form, func) {
+  constructor(form, func, value, selector) {
     this.form = form;
     this.func = func;
+    this.value = value;
+    this.selector = selector;
+
+    if(this.form && this.func) {
+      this.setEventListeners();
+    }
+    if(this.selector && this.value) {
+      this.form.querySelector(`${this.selector}`).value = this.value;
+    }
   }
 
   setEventListeners() {
